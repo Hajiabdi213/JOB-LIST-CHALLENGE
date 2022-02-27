52b8887edcbcf160e26b122697d6abc5504dabb1
Abdillahi <hajiabdi213@gmail.com>
2022-02-27 06:57:27 +0300

    styling classes were all grouped into the syle.css file

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,96 @@
 <script setup lang="ts">
 import JobBoard from "./components/JobBoard.vue";
 import data from "./assets/data.json";
-import { useTransitionState } from "@vue/runtime-core";
 //getting data from the JSON file
 const jobs = data;
 
-let jobsArray = [];
+let jobsArray: any = [];
 
 //adding data from the JSON to the jobs array
 for (const job of jobs) {
   jobsArray.push(job);
+  
 }
 
-console.log(jobsArray);
 
-// const jobArray= []
-// // console.log(jobs)
-// for (const job of jobs) {
-//     jobArray.push(job)
-// }
-console.log("The Job ");
+//filtering
+
+
+function filteringData(clickedItem: string) {
+  const jobDiv = document.querySelectorAll("#jobDiv");
+  const filteredBoxText = document.createElement("p");
+  const closeFilterIcon = document.createElement("button");
+  const clearAllButton=document.createElement("button")
+  // clearAllButton.className="clear-all-button text-teal-500 hover:underline transition absolute right-14"
+
+  // clearAllButton.textContent="clear"
+  closeFilterIcon.textContent = "X";
+  closeFilterIcon.className =
+    "close-filter-icon text-white font-bold bg-teal-500 p-2 -mr-2 rounded-r";
+  filteredBoxText.textContent = clickedItem;
+
+  filteredBoxText.className =
+    "text-teal-500 bg-teal-100 font-bold px-2 rounded m-2 flex gap-2 items-center";
+  const filterBox = document.querySelector("#filterBox");
+  if (filterBox?.textContent?.includes(clickedItem)) {
+    return;
+  }
+  
+
+  filteredBoxText.append(closeFilterIcon);
+  filterBox?.append(filteredBoxText);
+
+  jobDiv.forEach((div) => {
+    // console.log(div)
+
+    //removing unwanted Jobs to filter
+    if (!div.textContent.includes(clickedItem)) {
+      div.classList.add("remove-jobs");
+
+    }
+
+    
+    //close filter functionality
+
+    
+    closeFilterIcon.addEventListener("click", (e) => {
+      
+      div.classList.remove("remove-jobs");
+     
+
+       const closeFilterIconButton=document.querySelectorAll('.close-filter-icon')
+ 
+   
+      
+      closeFilterIcon.parentElement?.remove();
+      return
+    });
+  });
+}
+
+
 </script>
 
 <template>
-<header class="bg-teal-500 mb-12">
-  <img src="/images/bg-header-desktop.svg" alt="">
-</header>
+  <header class="bg-teal-500 mb-12">
+    <img class="hidden md:block w-full" src="/images/bg-header-desktop.svg" alt="" />
+    <img class=" md:hidden w-full h-48" src="/images/bg-header-mobile.svg" alt="" />
+  </header>
 
-<!-- all jobs goes here -->
+  <!-- filter box -->
+  <div
+    id="filterBox"
+    class="flex md:flex-row items-center bg-white shadow-md mx-10 rounded flex-wrap"
+  >
+  
+  
+  </div>
+
+  <!-- all jobs go here -->
   <JobBoard
     v-for="job in jobsArray"
     :companyName="job.company"
     :logoImg="job.logo"
-    :jobPostion="job.position"
     :postedTime="job.postedAt"
     :location="job.location"
     :contract="job.contract"
@@ -43,6 +101,7 @@ console.log("The Job ");
     :role="job.role"
     :isNew="job.isNew"
     :featured="job.featured"
+    @filteringData="filteringData"
   >
     <!-- <img src="./assets/images/insure.svg" alt=""> -->
     <!-- {{job.company}} -->
--- src/components/JobBoard.vue
+++ src/components/JobBoard.vue
@@ -1,69 +1,87 @@
 <script setup lang="ts">
 // defineProps<{company:string}>();
 
-
-
-type allMyProps={
-    companyName:string,
-    logoImg:string,
-    jobPosition:string,
-    postedTime:string,
-    contract:string,
-    location:string,
-    languages:string,
-    tools:string,
-    role:string,
-    level:string,
-    isNew:boolean,
-    featured:boolean
-
-
-
-}
- defineProps<allMyProps>();
-
+type allMyProps = {
+  companyName: string;
+  logoImg: string;
+  jobPosition: string;
+  postedTime: string;
+  contract: string;
+  location: string;
+  languages: string;
+  tools: string;
+  role: string;
+  level: string;
+  isNew: boolean;
+  featured: boolean;
+};
+defineProps<allMyProps>();
 </script>
 <template>
-
-    
-
-<!-- <div className="flex bg-white shadow-md m-4 p-6 rounded" > -->
-<div v-bind:class = "(featured)?'flex md:flex-row bg-white border-l-4 border-teal-500 border-solid shadow-md mx-10 my-16 p-6 rounded  flex-col':'flex md:flex-row bg-white shadow-md mx-10 my-16 p-6 rounded flex-col  '" >
+  <!-- <div className="flex bg-white shadow-md m-4 p-6 rounded" > -->
+  <div
+    id="jobDiv"
+    v-bind:class="
+      featured
+        ? 'job-card-featured'
+        : 'job-card'
+    "
+  >
     <!-- image -->
+    <!-- left -->
     <div class="flex items-center">
-            <img class=" rounded-3xl w-20   -mt-16 md:m-0" v-bind:src="logoImg" alt="">
-
+      <img class="company-logo" v-bind:src="logoImg" alt="" />
     </div>
     <!-- info -->
-    <!-- left -->
-    <div class="ml-4 flex flex-col justify-between ">
-        <h3 class="font-bold  text-teal-500">{{companyName}}
-            <span class="bg-teal-500 text-white text-sm m-2 py-1 px-2 rounded-full font-semibold uppercase" v-if="isNew"> New </span>
-            <span class="bg-gray-800 text-white text-sm m-2 py-1 px-2 rounded-full  font-semibold uppercase" v-if="featured"> Featured </span>
+    <div class="ml-4 flex flex-col justify-between">
+      <h3 class="font-bold text-teal-500">
+        {{ companyName }}
+        <span
+          class="isNewStyle"
+          v-if="isNew"
+        >
+          New
+        </span> 
+        <span
+          class="isFeaturedStyle"
+          v-if="featured"
+        >
+          Featured
+        </span>
+      </h3>
 
-        </h3>
-
-        <h2 class="font-bold text-xl my-2">{{jobPosition}}</h2>
-        <p class="text-gray-700">
-            {{postedTime}}
-            &#xb7;{{contract}}
-           &#xb7; {{location}}
-        </p>
+      <h2 class="font-bold text-xl my-2">{{ jobPosition }}</h2>
+      <p class="text-gray-700">
+        {{ postedTime }}
+        &#xb7;{{ contract }} &#xb7; {{ location }}
+      </p>
     </div>
 
     <!-- right side -->
-    <div class="flex items-center  mt-4 pt-4 border-t border-gray-500 border-solid flex-wrap md:ml-auto md:border-t-0 md:mt-0 md:pt-0">
-        <!-- <h1>{{languages}}</h1>
+    <div
+      class="right-side"
+    >
+      <!-- <h1>{{languages}}</h1>
         <h1>{{tools}}</h1> -->
-        
-        <span class="text-teal-500 bg-teal-100 font-bold p-2 rounded m-2">{{role}}</span>
-        <span class="text-teal-500 bg-teal-100 font-bold p-2 rounded m-2">{{level}}</span>
-        <span class="text-teal-500 bg-teal-100 font-bold p-2 rounded m-2" v-for="language in languages">{{language}}</span>
-        <span class="text-teal-500 bg-teal-100 font-bold p-2 rounded m-2" v-for="tool in tools">{{tool}}</span>
 
+      <span @click="$emit('filteringData', role)" class="filter-button">{{
+        role
+      }}</span>
+      <span @click="$emit('filteringData', level)" class="filter-button">{{
+        level
+      }}</span>
+      <span
+        @click="$emit('filteringData', language)"
+        class="filter-button"
+        v-for="language in languages"
+        >{{ language }}</span
+      >
+      <span
+        @click="$emit('filteringData', tool)"
+        class="filter-button"
+        v-for="tool in tools"
+        >{{ tool }}</span
+      >
     </div>
-   
-</div>
-    
+  </div>
 </template>
-
